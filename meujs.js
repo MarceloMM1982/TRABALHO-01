8a8f0ab2264837c1b5de7eb4c7ad7be3f8ba4647
MarceloMM1982 <upupmundi@gmail.com>
2025-05-17 21:12:37 -0300

    Add files via upload

---
+++ meujs.js
@@ -0,0 +1,6 @@
+const boasVindas= document.querySelector("#botao");
+const mensagem= document.querySelector("#resposta");
+boasVindas.addEventListener("click",resp);
+function resp (){
+    mensagem.innerHTML= "VEJA ABAIXO MEUS HOBBIES E LOCAIS QUE PRETENDO VIAJAR";
+}
